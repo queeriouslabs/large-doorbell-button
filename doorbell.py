043b073a3d43dc5d363d30e841345435588292c9
Rebecca Valentine <psygnisfive@yahoo.com>
2019-06-11 15:29:17 -0700

    adds prototype doorbell script

--- doorbell.py
+++ doorbell.py
@@ -1,5 +1,11 @@
 import evdev
 
-devices = [evdev.InputDevice(path) for path in evdev.util.list_devices()]
-for device in devices:
-  print(device.path, device.name, device.phys)
+print('Starting doorbell...')
+
+kb = evdev.InputDevice('/dev/input/event3')
+for event in kb.read_loop():
+  evcat = evdev.categorize(event)
+  if event.type == evdev.ecodes.EV_KEY and \
+     'KEY_YEN' == evcat.keycode and \
+     1 == evcat.event.value:
+    print('ding dong!')
