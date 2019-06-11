9fff1d694d60dafaf6d088ae83bea0cea6349f8d
Rebecca Valentine <psygnisfive@yahoo.com>
2019-06-11 15:43:23 -0700

    adds doorbell audio player

--- doorbell.py
+++ doorbell.py
@@ -1,11 +1,17 @@
 import evdev
+import simpleaudio
 
 print('Starting doorbell...')
 
+def should_ring(event):
+  evcat = evdev.categorize(event)
+  return event.type == evdev.ecodes.EV_KEY and \
+         'KEY_YEN' == evcat.keycode and \
+         1 == evcat.event.value
+
 kb = evdev.InputDevice('/dev/input/event3')
 for event in kb.read_loop():
-  evcat = evdev.categorize(event)
-  if event.type == evdev.ecodes.EV_KEY and \
-     'KEY_YEN' == evcat.keycode and \
-     1 == evcat.event.value:
-    print('ding dong!')
+  if should_ring(event):
+    wave_obj = simpleaudio.WaveObject.from_wave_file("tng_chime.wav")
+    play_obj = wave_obj.play()
+    play.obj.wait_done()
