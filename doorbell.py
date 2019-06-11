948b8d8f4bba54d2efbace99c841075b73f0f668
Rebecca Valentine <psygnisfive@yahoo.com>
2019-06-11 15:49:01 -0700

    adds doorbell time constraints

--- doorbell.py
+++ doorbell.py
@@ -3,15 +3,25 @@ import simpleaudio
 
 print('Starting doorbell...')
 
-def should_ring(event):
+def within_valid_timerange(last_event_timestamp, event):
+  return last_event_timestamp + 5000 <= event.timestamp()
+
+def is_enter_keydown(event):
   evcat = evdev.categorize(event)
+  
   return event.type == evdev.ecodes.EV_KEY and \
          'KEY_YEN' == evcat.keycode and \
          1 == evcat.event.value
 
+def should_ring(last_event_timestamp, event):
+  return within_valid_timerange(last_event_timestamp, event) and \
+         is_enter_keydown(event)
+
 kb = evdev.InputDevice('/dev/input/event3')
+last_event_timestamp = None
 for event in kb.read_loop():
-  if should_ring(event):
+  if should_ring(last_event_timestamp, event):
+    last_event_timestamp = event.timestamp()
     wave_obj = simpleaudio.WaveObject.from_wave_file("tng_chime.wav")
     play_obj = wave_obj.play()
     play_obj.wait_done()
