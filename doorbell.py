7b8f7dd904c62ffa4ba79041bf7f9ee6a8e6c44e
Rebecca Valentine <psygnisfive@yahoo.com>
2019-06-11 16:00:22 -0700

    adds doorbell finder

--- doorbell.py
+++ doorbell.py
@@ -1,8 +1,6 @@
 import evdev
 import simpleaudio
 
-print('Starting doorbell...')
-
 def within_valid_timerange(last_event_timestamp, event):
   return not last_event_timestamp or \
              last_event_timestamp + 2 <= event.timestamp()
@@ -18,11 +16,21 @@ def should_ring(last_event_timestamp, event):
   return within_valid_timerange(last_event_timestamp, event) and \
          is_enter_keydown(event)
 
-kb = evdev.InputDevice('/dev/input/event3')
-last_event_timestamp = None
-for event in kb.read_loop():
-  if should_ring(last_event_timestamp, event):
-    last_event_timestamp = event.timestamp()
-    wave_obj = simpleaudio.WaveObject.from_wave_file("tng_chime.wav")
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
+devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+button_path = None
+for device in devices:
+  if 'SIGMACHI' in device.name:
+    button_path = device.path #print(device.path, device.name, device.phys)
+
+if not button_path:
+  print('No doorbell. :(')
+else:
+  print('Found doorbell: ' + button_path)
+  button = evdev.InputDevice(button_path)
+  last_event_timestamp = None
+  for event in button.read_loop():
+    if should_ring(last_event_timestamp, event):
+      last_event_timestamp = event.timestamp()
+      wave_obj = simpleaudio.WaveObject.from_wave_file("tng_chime.wav")
+      play_obj = wave_obj.play()
+      play_obj.wait_done()
