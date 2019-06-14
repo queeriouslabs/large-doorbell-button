22e066fbb0c899ef905ac2255807151ca18dcee4
Rebecca Valentine <psygnisfive@yahoo.com>
2019-06-13 21:01:43 -0700

    adds service installer

--- install_service.sh
+++ install_service.sh
@@ -1,11 +1,3 @@
-# large-doorbell-button
-
-Sourcecode to run the large doorbell button.
-
-# Installing as a service
-
-run install_service.sh:
-
 sudo cp doorbell.service /etc/systemd/system/
 sudo chmod 644 /etc/systemd/system/doorbell.service
 sudo systemctl start doorbell
