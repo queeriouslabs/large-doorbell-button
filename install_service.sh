6f10c19c6bbf246f6811bfda267d9a4336f39982
Rebecca Valentine <psygnisfive@yahoo.com>
2019-06-13 21:20:57 -0700

    enable the right service

--- install_service.sh
+++ install_service.sh
@@ -1,6 +1,6 @@
 sudo cp doorbell.service /etc/systemd/system/
 sudo chmod 644 /etc/systemd/system/doorbell.service
 sudo systemctl daemon-reload
-sudo systemctl enable myservice
+sudo systemctl enable doorbell
 sudo systemctl start doorbell
 sudo systemctl status doorbell
