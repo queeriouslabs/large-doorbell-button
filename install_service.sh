35364132ca364aa9fe74aeccb5372739eaff8300
Rebecca Valentine <psygnisfive@yahoo.com>
2019-06-13 21:08:15 -0700

    adds daemon-reload

--- install_service.sh
+++ install_service.sh
@@ -1,4 +1,5 @@
 sudo cp doorbell.service /etc/systemd/system/
 sudo chmod 644 /etc/systemd/system/doorbell.service
+sudo systemctl daemon-reload
 sudo systemctl start doorbell
 sudo systemctl status doorbell
