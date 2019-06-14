91bdf7f852b89e55c6f0412110c7733c5ef76e8f
Rebecca Valentine <psygnisfive@yahoo.com>
2019-06-13 20:40:51 -0700

    set the voyager chime to be the doorbell

--- doorbell.py
+++ doorbell.py
@@ -32,6 +32,6 @@ else:
   for event in button.read_loop():
     if should_ring(last_event_timestamp, event):
       last_event_timestamp = event.timestamp()
-      wave_obj = simpleaudio.WaveObject.from_wave_file("tng_chime.wav")
+      wave_obj = simpleaudio.WaveObject.from_wave_file("voy_door_chime.wav")
       play_obj = wave_obj.play()
       play_obj.wait_done()
