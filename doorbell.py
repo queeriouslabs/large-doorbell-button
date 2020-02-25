22167b6f9d88fabec8d6b5b041fc857348f45c68
Beka Valentine <beka@gothdyke.mom>
2020-02-25 14:26:42 -0800

    Adds a try-catch in case there are weird errors in the event loop.

--- doorbell.py
+++ doorbell.py
@@ -29,9 +29,13 @@ else:
   print('Found doorbell: ' + button_path)
   button = evdev.InputDevice(button_path)
   last_event_timestamp = None
-  for event in button.read_loop():
-    if should_ring(last_event_timestamp, event):
-      last_event_timestamp = event.timestamp()
-      wave_obj = simpleaudio.WaveObject.from_wave_file("voy_door_chime.wav")
-      play_obj = wave_obj.play()
-      play_obj.wait_done()
+  while True:
+    try:
+      for event in button.read_loop():
+        if should_ring(last_event_timestamp, event):
+          last_event_timestamp = event.timestamp()
+          wave_obj = simpleaudio.WaveObject.from_wave_file("voy_door_chime.wav")
+          play_obj = wave_obj.play()
+          play_obj.wait_done()
+    catch:
+      pass
